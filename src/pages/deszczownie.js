0e4d13eb0c3e0d36a37d94ff0bf0ded20418766a
Mateusz-Wójcik <mateusz-wojcik@hotmail.com>
2022-01-29 00:10:15 +0100

    common section for product page

--- src/pages/deszczownie.js
+++ src/pages/deszczownie.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
 import { Section } from "../components/layout/Layout.styles";
+import Partners from "../components/sections/Partners";
+import Cta from "../components/sections/Cta";
 
 const ContactPage = () => {
   return (
     <Layout>
       <Section>Test page</Section>
+      <Cta />
+      <Partners />
     </Layout>
   );
 };
